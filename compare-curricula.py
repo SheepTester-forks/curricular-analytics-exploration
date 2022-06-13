3c02131a0629e543a3ef5cadec10e03524751531
Sean <ussa.say@gmail.com>
2022-06-13 12:39:07 -0700

    Update code to consider duplicate course names

--- compare-curricula.py
+++ compare-curricula.py
@@ -1,16 +1,50 @@
 import itertools
 import re
+from typing import List, Tuple
 from parse import major_plans
 
 colleges = ["RE", "MU", "TH", "WA", "FI", "SI", "SN"]
 ignored = ["GE/DEI", "DEI/GE", "DEI"]
+
+
+def difference(a: List[str], b: List[str]) -> Tuple[List[str], List[str]]:
+    """
+    Takes two sorted lists, which may have duplicate items, and returns tuple of
+    a list of items only in `a` and items only in `b`.
+    """
+    a_exclusive: List[str] = []
+    b_exclusive: List[str] = []
+    i = 0
+    j = 0
+    while i < len(a) and j < len(b):
+        if a[i] == b[j]:
+            i += 1
+            j += 1
+        elif a[i] < b[j]:
+            a_exclusive.append(a[i])
+            i += 1
+        else:
+            b_exclusive.append(b[j])
+            j += 1
+    a_exclusive += a[i:]
+    b_exclusive += b[j:]
+    return a_exclusive, b_exclusive
+
+
+def display_set(ls: List[str]) -> str:
+    """
+    Represent a list like a set so the Git diff for comparisons.txt isn't all
+    just changing curly braces to square brackets.
+    """
+    return f"{{{repr(ls)[1:-1]}}}" if ls else "set()"
+
 
 oddity_scores = {college: 0 for college in colleges}
 deviation_scores = {college: 0 for college in colleges}
 for major_plan in major_plans.values():
     try:
         curricula = {
-            college: {
+            college: sorted(
                 re.sub(
                     r"( ?/ ?(awp|dei|sixth practicum)| \(dei approved\)|\*\* ?/elective)$",
                     "",
@@ -18,7 +52,7 @@ for major_plan in major_plans.values():
                 )
                 for course in major_plan.curriculum(college)
                 if course.course_code not in ignored
-            }
+            )
             for college in colleges
         }
     except KeyError as e:
@@ -35,8 +69,9 @@ for major_plan in major_plans.values():
                 print(f"= Discrepancies for {major_plan.major_code} =")
                 header_printed = True
             print(f"== {clg1} vs {clg2} ==")
-            print(f"[{clg1}] {curr1 - curr2}")
-            print(f"[{clg2}] {curr2 - curr1}")
+            only1, only2 = difference(curr1, curr2)
+            print(f"[{clg1}] {display_set(only1)}")
+            print(f"[{clg2}] {display_set(only2)}")
             local_scores[clg1] += 1
             local_scores[clg2] += 1
             oddity_scores[clg1] += 1
