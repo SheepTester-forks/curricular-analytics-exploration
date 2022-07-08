74808cc2590e3e6a1f6efdecbb9c5f3d31b981b9
Sean <ussa.say@gmail.com>
2022-07-07 21:24:13 -0700

    Output CSVs for all majors

---
+++ output_all.py
@@ -0,0 +1,20 @@
+import os
+from output import MajorOutput
+from parse import major_plans
+
+
+def csv_to_file(csv: str, path: str) -> None:
+    with open(path, "w") as file:
+        file.write(csv)
+
+
+for major_code, plan in major_plans.items():
+    os.makedirs(f"./files/output/{major_code}/", exist_ok=True)
+
+    output = MajorOutput(major_code)
+    csv_to_file(output.output(), f"./files/output/{major_code}/curriculum.csv")
+    for college_code in plan.plans.keys():
+        csv_to_file(
+            output.output(college_code),
+            f"./files/output/{major_code}/{college_code}.csv",
+        )
