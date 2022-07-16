a9b8d2e78e43bc69169fd3b7c4d080d7d8d96a7f
Sean <ussa.say@gmail.com>
2022-07-15 18:12:12 -0700

    Display which majors have courses with redundant prereqs

--- redundant_prereq_check.py
+++ redundant_prereq_check.py
@@ -1,5 +1,6 @@
-from typing import Set
-from parse import CourseCode, prereqs
+from typing import Dict, Set
+from parse import CourseCode, major_plans, prereqs
+from parse_course_name import parse_course_name
 
 flattened = {
     course_code: {
@@ -17,10 +18,31 @@ def get_nested_prereqs(code: CourseCode, cache: Set[CourseCode]) -> None:
                 get_nested_prereqs(prereq, cache)
 
 
+redundancies: Dict[CourseCode, Set[CourseCode]] = {}
+
 for course_code, requisites in flattened.items():
     prereqs_of_prereqs: Set[CourseCode] = set()
     for prereq in requisites:
         get_nested_prereqs(prereq, prereqs_of_prereqs)
     redundant = {code for code in requisites if code in prereqs_of_prereqs}
-    if len(redundant) > 0:
-        print(f"[{course_code}] {redundant}")
+    if redundant:
+        redundancies[course_code] = redundant
+
+print(redundancies)
+
+for major_code, major in major_plans.items():
+    need_prereq_removal: Set[CourseCode] = set()
+    for course in major.curriculum():
+        parsed = parse_course_name(course.course_title)
+        if parsed is not None:
+            subject, number, has_lab = parsed
+            if (subject, number) in redundancies:
+                need_prereq_removal.add((subject, number))
+            if has_lab is not None and (subject, number + has_lab) in redundancies:
+                need_prereq_removal.add((subject, number + has_lab))
+    if need_prereq_removal:
+        display = " | ".join(
+            f"{subject} {number} <- {', '.join(map( ' '.join, redundancies[subject, number]))}"
+            for subject, number in need_prereq_removal
+        )
+        print(f"[{major_code}] {display}")
