13cd0ec498bad62520c5777ac45cf6226fc8a854
Sean <ussa.say@gmail.com>
2022-08-01 16:42:42 -0700

    Get course overlap between every major for every year

--- CourseOverlap.jl
+++ CourseOverlap.jl
@@ -9,12 +9,19 @@ import .Utils: writerow
 plans = get_plans()
 
 open("./files/course_overlap.csv", "w") do file
+  writerow(file, [
+    "Year",
+    "Base major",
+    "Other major",
+    "Percent of base major's courses in other major",
+  ])
+
   for year in 2015:2050
     if year ∉ keys(plans)
       break
     end
     curricula = Dict(
-      major => [
+      major => Set([
         course
         for term in degree_plans[first(
           college
@@ -23,10 +30,20 @@ open("./files/course_overlap.csv", "w") do file
         )]
         for course in term
         if course.for_major
-      ]
+      ])
       for (major, degree_plans) in plans[year]
     )
     majors = sort(collect(keys(curricula)))
+    for base in majors
+      for other in majors
+        writerow(file, [
+          string(year),
+          base,
+          other,
+          string(length(curricula[base] ∩ curricula[other]) / length(curricula[base]))
+        ])
+      end
+    end
   end
 end
 
