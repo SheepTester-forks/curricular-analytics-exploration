42428069287439f4e010ccd5cf4155d3243455bb
Sean <ussa.say@gmail.com>
2022-08-16 15:57:58 -0700

    Output links with department and school name as well

--- curricula_index.py
+++ curricula_index.py
@@ -1,15 +1,21 @@
+from departments import departments, dept_schools
+from parse import major_codes
 from upload import track_uploaded_curricula
 
-print("Year,Major,URL")
+print("School,Department,Major,Year,URL")
 
 for year in range(2015, 2023):
     with track_uploaded_curricula(year) as curricula:
         for major_code, curriculum_id in curricula.items():
+            department = major_codes()[major_code].department
+            department_name = departments.get(department) or "UNKNOWN"
             print(
                 ",".join(
                     [
-                        str(year),
+                        dept_schools.get(department) or department_name,
+                        department_name,
                         major_code,
+                        str(year),
                         f"https://curricularanalytics.org/curriculums/{curriculum_id}",
                     ]
                 )
