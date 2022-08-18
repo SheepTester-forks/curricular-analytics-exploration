d76158cf6160ce451ee0dd30c9a87a043ebc4682
Sean <ussa.say@gmail.com>
2022-08-18 13:28:41 -0700

    Distinguish between or and not or

--- flag_issues.py
+++ flag_issues.py
@@ -4,9 +4,16 @@ from parse import ParsedCourse, major_plans
 
 def check_plan(name: str, plan: List[ParsedCourse]) -> None:
     course_codes = [course.course_code for course in plan if course.course_code]
+    course_titles = {
+        course.course_code: course.course_title for course in plan if course.course_code
+    }
     for i, code in enumerate(course_codes):
         if code in course_codes[0:i]:
-            print(f"[{name}] duplicate course {code}")
+            title = course_titles[code]
+            if any(char in title for char in ["/", "or", "OR"]):
+                print(f"[{name}] multiple options for {code} {title}")
+            else:
+                print(f"[{name}] duplicate course {code} {title}")
 
 
 for major_code, plans in major_plans(2022).items():
