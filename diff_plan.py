4d5eb66b44bd5ca8a362ab02243ef700370d3c08
Sean <ussa.say@gmail.com>
2022-08-24 11:07:17 -0700

    Display diff with COLOURS

--- diff_plan.py
+++ diff_plan.py
@@ -1,15 +1,35 @@
-from typing import List
+from typing import List, NamedTuple
 
 from parse import RawCourse, major_plans
 
 
-def diff(old: List[RawCourse], new: List[RawCourse]):
+class DiffResults(NamedTuple):
+    added: List[RawCourse]
+    removed: List[RawCourse]
+
+    def print(self) -> None:
+        for course in self.removed:
+            print(f"\033[31m- {course.course_title}")
+        for course in self.added:
+            print(f"\033[32m+ {course.course_title}")
+
+
+def diff(old: List[RawCourse], new: List[RawCourse]) -> DiffResults:
     old_only = [course for course in old if course not in new]
     new_only = [course for course in new if course not in old]
-    print(old_only)
-    print(new_only)
+    return DiffResults(new_only, old_only)
 
 
-diff(
-    major_plans(2020)["BE25"].raw_plans["RE"], major_plans(2021)["BE25"].raw_plans["RE"]
-)
+if __name__ == "__main__":
+    # https://stackoverflow.com/questions/287871/how-do-i-print-colored-text-to-the-terminal#comment113206663_21786287
+    import os
+
+    os.system("color")
+
+    diff(
+        major_plans(2020)["BE25"].raw_plans["RE"],
+        major_plans(2021)["BE25"].raw_plans["RE"],
+    ).print()
+
+# do it for every year
+# just print the added and removed courses
