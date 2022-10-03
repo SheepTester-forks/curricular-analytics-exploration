875aeab4b2ca6997615590228b7e6fda37a2943f
Sean <ussa.say@gmail.com>
2022-10-03 16:33:20 -0700

    thinking about how to do redundant prereqs, I think I'll just assume all alts are taken

--- redundant_prereq_courses.py
+++ redundant_prereq_courses.py
@@ -1,11 +1,52 @@
-from parse import prereqs
+from typing import List, NamedTuple
+from parse import CourseCode, prereqs
 
 
-course_codes = prereqs("FA22").items()
+# PHYS 1B requires [MATH 10B, MATH 20B], among others. PHYS 2B requires [MATH
+# 20B, MATH 20C, MATH 31BH], among others. PHYS 1C requires [PHYS 1B, PHYS 2B]
+# and [MATH 10B, MATH 20B]. Is this redundant? Taking PHYS 2B does not imply you
+# have credit for MATH 20B. MATH 20C implies MATH 20B, but MATH 31BH implies
+# MATH 31AH, which doesn't imply anything. You could have credit for MATH 31BH
+# and not meet the requirements for PHYS 1C. Is that a good thing?
 
-# 1. Traverse each course's prerequisites to note down what courses a student
-#    must've taken
-# 2. Of the course's direct prerequisites, if they show up in a different
-#    prereq's descendants, then it is redundant.
-# 3. TODO: What about lists of alternative courses? I could keep them as ORs,
-#    then if there is a partial match then I could flag them differently.
+# I'll just assume that *every* alternative is taken.
+
+course_prereqs = {
+    course_code: [alt.course_code for req in prereqs for alt in req]
+    for course_code, prereqs in prereqs("FA22").items()
+}
+
+
+class TakenCourse(NamedTuple):
+    course: CourseCode
+    satisfies: CourseCode
+
+
+def take_prereq(course_code: CourseCode, target: List[TakenCourse]) -> None:
+    for course in course_prereqs[course_code]:
+        target.append(TakenCourse(course, course_code))
+        take_prereq(course, target)
+
+
+def redundant_prereqs(course_code: CourseCode) -> List[TakenCourse]:
+    implied: List[TakenCourse] = []
+    prereqs = course_prereqs[course_code]
+    for course in prereqs:
+        take_prereq(course, implied)
+    redundant: List[TakenCourse] = []
+    for course in prereqs:
+        for taken in implied:
+            if course == taken.course:
+                redundant.append(taken)
+                break
+    return redundant
+
+
+def main() -> None:
+    print("PHYS 1C")
+    for course, satisfies in redundant_prereqs(CourseCode("PHYS", "1C")):
+        print(f"{course} (-> {satisfies})")
+
+
+if __name__ == "__main__":
+    main()
