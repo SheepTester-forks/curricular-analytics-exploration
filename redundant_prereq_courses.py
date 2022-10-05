5a9a693ef10b47821b922497f37be1e6c3d5b7ec
Sean <ussa.say@gmail.com>
2022-10-04 19:02:17 -0700

    Partitioned lists

--- redundant_prereq_courses.py
+++ redundant_prereq_courses.py
@@ -1,5 +1,6 @@
-from typing import List, NamedTuple, Optional, Set, Tuple
+from typing import Dict, List, NamedTuple, Optional, Set, Tuple
 from parse import CourseCode, prereqs
+from util import add_entry, merge_partition, sorted_partition
 
 
 # PHYS 1B requires [MATH 10B, MATH 20B], among others. PHYS 2B requires [MATH
@@ -22,9 +23,9 @@ PrereqChain = Tuple[CourseCode, ...]
 
 
 class State(NamedTuple):
-    taken: List[PrereqChain]
+    taken: Dict[CourseCode, List[PrereqChain]]
     explored: Set[CourseCode]
-    nonexistent: Set[PrereqChain]
+    nonexistent: Dict[CourseCode, List[PrereqChain]]
 
 
 def take_prereq(course_code: CourseCode, target: State, satisfies: PrereqChain) -> None:
@@ -32,42 +33,49 @@ def take_prereq(course_code: CourseCode, target: State, satisfies: PrereqChain) 
         return
     target.explored.add(course_code)
     for course in course_prereqs[course_code]:
-        chain = course, *satisfies
-        target.taken.append(chain)
+        add_entry(target.taken, course, satisfies)
         if course in course_prereqs:
-            take_prereq(course, target, chain)
+            take_prereq(course, target, (course, *satisfies))
         else:
-            target.nonexistent.add(chain)
+            add_entry(target.nonexistent, course, satisfies)
 
 
 def redundant_prereqs(
-    course_code: CourseCode, nonexistent: Optional[Set[PrereqChain]] = None
-) -> List[PrereqChain]:
-    state = State([], set(), set())
+    course_code: CourseCode,
+    nonexistent: Optional[Dict[CourseCode, List[PrereqChain]]] = None,
+) -> Dict[CourseCode, List[PrereqChain]]:
+    state = State({}, set(), {})
     prereqs = course_prereqs[course_code]
     for course in prereqs:
         if course in course_prereqs:
             take_prereq(course, state, (course, course_code))
         else:
-            state.nonexistent.add((course, course_code))
+            add_entry(state.nonexistent, course, (course_code,))
     if nonexistent is not None:
-        nonexistent |= state.nonexistent
-    return [taken for course in prereqs for taken in state.taken if course == taken[0]]
+        merge_partition(nonexistent, state.nonexistent)
+    return {course: state.taken[course] for course in prereqs if course in state.taken}
 
 
 def main() -> None:
-    nonexistent: Set[PrereqChain] = set()
+    nonexistent: Dict[CourseCode, List[PrereqChain]] = {}
     for course in sorted(course_prereqs):
         redundant = redundant_prereqs(course, nonexistent)
         if not redundant:
             continue
         print(f"[{course}]")
-        for course, *satisfies in redundant:
-            chain = " → ".join(str(course) for course in satisfies)
-            print(f"Has redundant prereq {course}, which was already taken for {chain}")
+        for course, chains in sorted_partition(redundant):
+            display_chains = ", ".join(
+                " → ".join(str(course) for course in chain) for chain in chains
+            )
+            print(
+                f"Has redundant prereq {course}, which was already taken for {display_chains}"
+            )
     print("[Nonexistent courses]")
-    for course, satisfies, *_ in nonexistent:
-        print(f"{satisfies} requires {course}, which doesn't exist")
+    for course, chains in sorted_partition(nonexistent):
+        display_chains = ", ".join(
+            " → ".join(str(course) for course in chain) for chain in chains
+        )
+        print(f"Nonexistent {course} required by {display_chains}")
 
 
 if __name__ == "__main__":
---
+++ util.py
@@ -0,0 +1,78 @@
+"""
+Utility functions for a partitioned list, which is what I will call a dictionary
+that maps keys to lists of values with the same key.
+"""
+
+from typing import Any, Dict, Iterator, List, Protocol, Tuple, TypeVar
+
+
+K = TypeVar("K")
+V = TypeVar("V")
+
+
+# Stolen from the typing for `sorted`
+_T_contra = TypeVar("_T_contra", contravariant=True)
+
+
+class SupportsDunderLT(Protocol[_T_contra]):
+    def __lt__(self, __other: _T_contra) -> bool:
+        ...
+
+
+CompK = TypeVar("CompK", bound=SupportsDunderLT[Any])
+
+
+def add_entries(target: Dict[K, List[V]], key: K, values: List[V]) -> None:
+    """
+    Insert a list of values with the same key into a partitioned list.
+    """
+    existing = target.get(key)
+    if existing:
+        existing += values
+    else:
+        target[key] = [*values]
+
+
+def add_entry(target: Dict[K, List[V]], key: K, value: V) -> None:
+    """
+    Insert an entry into a partitioned list.
+    """
+    values = target.get(key)
+    if values:
+        values.append(value)
+    else:
+        target[key] = [value]
+
+
+def partition(iterable: Iterator[Tuple[K, V]]) -> Dict[K, List[V]]:
+    """
+    Partitions a stream of values based on a key. The result maps keys to lists
+    of values with the same key.
+
+    The following example maps course code subjects to a list of course code
+    numbers per subject.
+
+    ```py
+    subjects = partition((subject, number) for subject, number in course_codes)
+    ```
+    """
+    result: Dict[K, List[V]] = {}
+    for key, value in iterable:
+        add_entry(result, key, value)
+    return result
+
+
+def merge_partition(
+    target: Dict[K, List[V]], additional: Dict[K, List[V]]
+) -> Dict[K, List[V]]:
+    """
+    Merges a partitioned list `additional` into an existing partitioned list
+    `target`, mutating `target`.
+    """
+    for key, values in additional.items():
+        add_entries(target, key, values)
+    return target
+
+
+def sorted_partition(dictionary: Dict[CompK, List[V]]) -> List[Tuple[CompK, List[V]]]:
+    return [*sorted(dictionary.items(), key=lambda entry: entry[0])]
