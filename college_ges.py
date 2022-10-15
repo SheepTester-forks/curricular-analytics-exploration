479219f3a3b9d6d97b03919764e6d706cd07fda8
Sean <ussa.say@gmail.com>
2022-10-14 20:08:46 -0700

    Make "ELECTIVE" case insensitive and omit UN* majors

--- college_ges.py
+++ college_ges.py
@@ -4,6 +4,8 @@ from parse import major_plans
 print("Major," + ",".join(college_names[college] for college in college_codes))
 
 for major_code, plans in major_plans(2022).items():
+    if major_code.startswith("UN"):
+        continue
     print(major_code, end="")
     for college in college_codes:
         if college not in plans.colleges:
@@ -12,7 +14,7 @@ for major_code, plans in major_plans(2022).items():
         extra_ge_units = sum(
             course.units
             for course in plans.plan(college)
-            if course.course_title != "ELECTIVE" and not course.for_major
+            if course.course_title.upper() != "ELECTIVE" and not course.for_major
         )
         print(f",{extra_ge_units}", end="")
     print()
