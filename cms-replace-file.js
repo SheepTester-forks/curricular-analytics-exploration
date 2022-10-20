0ce4105fac1dbd7f9316dcf0a3b973c977df97fd
Sean <ussa.say@gmail.com>
2022-10-20 09:40:08 -0700

    A better way to update the HTML reports on the CMS?

---
+++ cms-replace-file.js
@@ -0,0 +1,58 @@
+// Run in the CMS folder view to replace HTML reports
+
+async function writeFile (fileId, contents, message) {
+  const submittedFormIdentifier = (
+    await fetch(`/entity/edit.act?id=${fileId}&type=file`).then(r => r.text())
+  ).match(
+    /<input type="hidden" name="submittedFormIdentifier" value="([0-9a-z]+)"\/>/
+  )[1]
+
+  let formData = new FormData()
+  formData.set('submittedFormIdentifier', submittedFormIdentifier)
+  formData.set('id', fileId)
+  formData.set('bytesAsString', contents)
+
+  await fetch('/entity/submitfile.act?saveAs=draft', {
+    body: formData,
+    method: 'POST'
+  })
+
+  const submitId = (
+    await fetch(`/entity/open.act?type=file&id=${fileId}`).then(r => r.text())
+  ).match(/<input type="hidden" name="id" value="([0-9a-z]+)"\/>/)[1]
+
+  formData = new FormData()
+  formData.set('type', 'file')
+  formData.set('id', submitId)
+  formData.set('dataCheckSettings.comments', message)
+
+  await fetch('/entity/submitDraft.act?submitButtonType=submit', {
+    method: 'POST',
+    body: formData
+  })
+}
+
+for (const assetRow of document.querySelectorAll(
+  '#DataTables_Table_0_wrapper td:nth-child(2)'
+)) {
+  const assetId = new URL(assetRow.children[0].href).searchParams.get('id')
+  const label = document.createElement('label')
+  const fileInput = Object.assign(document.createElement('input'), {
+    type: 'file'
+  })
+  label.append('Replace file: ', fileInput)
+  assetRow.append(label)
+
+  fileInput.addEventListener('change', async () => {
+    if (fileInput.files[0]) {
+      fileInput.disabled = true
+      await writeFile(
+        assetId,
+        await fileInput.files[0].text(),
+        prompt('Change message')
+      )
+      fileInput.value = ''
+      fileInput.disabled = false
+    }
+  })
+}
