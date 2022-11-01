063d8ec9dbe6a1a4573709071b054655f64b995a
Sean <ussa.say@gmail.com>
2022-10-31 17:15:52 -0700

    Test course name parsing

--- course_names.py
+++ course_names.py
@@ -1,42 +1,23 @@
+"""
+python3 course_names.py > course_names.txt
+"""
+
 import re
 from parse import major_plans
+from ucsd import clean_course_title, parse_course_name
 
 course_names = {
-    course.course_title.strip("^* "): f"{major_plan.major_code} {college}"
-    for major_plan in major_plans(2021).values()
+    clean_course_title(course.course_title): f"{major_plan.major_code} {college}"
+    for year in range(2015, 2023)
+    for major_plan in major_plans(year).values()
     for college in major_plan.colleges
     for course in major_plan.plan(college)
 }
 
 
-def get_course_code(name: str) -> str:
-    if name.startswith("ADV. CHEM"):
-        # ADV. CHEM 1-3 are not CHEM 1-3 but rather an elective category, so
-        # don't treat them like courses
-        return ""
-
-    # Remove International Studies' Disciplinary Focus prefix
-    name = re.sub(r"DF-?\d - ", "", name)
-
-    # Special case for physics labs or language analysis sections, which are
-    # often merged with its corresponding course, eg "PHYS 2C & 2CL"
-    match = re.search(
-        r"\b([A-Z]{2,4}) *(\d+[A-Z]{0,2})(?: *[&/] *\d?[A-Z]([LX]))?", name
-    )
-    if match:
-        subject, number, has_lab = match.group(1, 2, 3)
-        if subject in ["IE", "RR"]:
-            # International Studies' "Interdisciplinary Elective/Regional
-            # Requirement" is not a course name, but it might be worth still
-            # returning a prefix/department for these
-            return ""
-        if has_lab:
-            return f"{subject} {number}, {subject} {number}{has_lab}"
-        return f"{subject} {number}"
-    return ""
-
-
 for name in sorted(course_names):
-    if not re.match(r"^[A-Z]{2,4} \d{1,3}[A-Z]?$", name):
-        # https://stackoverflow.com/a/5676676
-        print(f"[{course_names[name]}] {name}".ljust(80) + get_course_code(name))
+    # Remove obvious course codes and non-course codes for brevity
+    if re.search(r"\d", name) and not re.match(r"^[A-Z]{2,4} \d{1,3}[A-Z]?$", name):
+        parsed = parse_course_name(name, 0)
+        display = str(parsed[0][0]) if parsed[0][0] else ""
+        print(f"[{course_names[name]}] {name}".ljust(80) + display)
