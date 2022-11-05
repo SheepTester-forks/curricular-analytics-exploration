0dc995a4e3ee378bc97e66087894c2b57cde7901
Sean <ussa.say@gmail.com>
2022-11-04 17:01:15 -0700

    Add and remove taken courses

--- reports/prereq-tree.tsx
+++ reports/prereq-tree.tsx
@@ -6,13 +6,76 @@
 import { render } from 'https://esm.sh/preact@10.11.2'
 import { useState } from 'https://esm.sh/preact@10.11.2/hooks'
 
-type AppProps = {
-  prereqs: Record<string, string[][]>
+type Prereqs = Record<string, string[][]>
+
+type CourseAdderProps = {
+  courseCodes: string[]
+  selected: string[]
+  onSelected: (selected: string[]) => void
 }
-function App ({}: AppProps) {
+function CourseAdder ({ courseCodes, selected, onSelected }: CourseAdderProps) {
+  const [query, setQuery] = useState('')
+
   return (
     <>
-      <p>Hi lmao</p>
+      <ul>
+        {selected.map(courseCode => (
+          <li key={courseCode}>
+            {courseCode}
+            <button
+              onClick={() => {
+                onSelected(selected.filter(code => code !== courseCode))
+              }}
+            >
+              ×
+            </button>
+          </li>
+        ))}
+      </ul>
+      <form
+        onSubmit={e => {
+          const courseCode = query.toUpperCase().trim().replace(/\s+/, ' ')
+          if (courseCodes.includes(courseCode)) {
+            if (!selected.includes(courseCode)) {
+              onSelected([...selected, courseCode])
+            }
+            setQuery('')
+          }
+          e.preventDefault()
+        }}
+      >
+        <input
+          type='search'
+          name='course-code'
+          list='courses'
+          value={query}
+          onInput={e => {
+            setQuery(e.currentTarget.value)
+          }}
+        />
+      </form>
+      <datalist id='courses'>
+        {courseCodes.map(code => (
+          <option value={code} key={code} />
+        ))}
+      </datalist>
+    </>
+  )
+}
+
+type AppProps = {
+  prereqs: Prereqs
+}
+function App ({ prereqs }: AppProps) {
+  const [courses, setCourses] = useState<string[]>([])
+
+  return (
+    <>
+      <CourseAdder
+        courseCodes={Object.keys(prereqs)}
+        selected={courses}
+        onSelected={setCourses}
+      />
     </>
   )
 }
