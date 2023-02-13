673a45c2b33cff494eba3d60ae64a4de546e976c
Sean <ussa.say@gmail.com>
2023-02-13 12:30:47 -0800

    add placeholder text for the prereq editors, minimal design

--- reports/plan-editor.tsx
+++ reports/plan-editor.tsx
@@ -614,23 +614,34 @@ function CustomCourse ({
   isNew
 }: CustomCourseProps) {
   return (
-    <li>
+    <li class='custom-course'>
       <input
         class='custom-course-input'
         type='text'
+        placeholder={isNew ? 'Type a course code here' : 'Course code'}
         value={name}
         onInput={e => onName(e.currentTarget.value)}
       />
       {!isNew && (
-        <ul>
+        <ol class='custom-course-prereqs'>
           {[...reqs, []].map((alts, i) => (
             <li key={i}>
-              <ul class='custom-prereq-req'>
+              <ul class='custom-prereq-alts'>
                 {[...alts, ''].map((alt, j) => (
                   <li key={j}>
                     <input
                       class='custom-prereq-input'
                       type='text'
+                      list='courses'
+                      placeholder={
+                        i === reqs.length
+                          ? 'New requirement'
+                          : j === 0
+                          ? 'Prerequsite'
+                          : j === alts.length
+                          ? 'Add alternate'
+                          : 'Alternate'
+                      }
                       value={alt}
                       onInput={e =>
                         onReqs(
@@ -648,12 +659,13 @@ function CustomCourse ({
                         )
                       }
                     />
+                    {j < alts.length - 1 && ' OR '}
                   </li>
                 ))}
               </ul>
             </li>
           ))}
-        </ul>
+        </ol>
       )}
     </li>
   )
@@ -710,7 +722,7 @@ function PrereqSidebar ({ prereqs, onPrereqs, plan }: PrereqSidebarProps) {
         For advisors creating a new major or if there's a missing course. Create
         a course with an existing course code to change its prerequisites.
       </p>
-      <ul>
+      <ul class='custom-courses'>
         {[...custom, { name: '', reqs: [] }].map(({ name, reqs }, i) => (
           <CustomCourse
             name={name}
