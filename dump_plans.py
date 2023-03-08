3488ae62518a1bdab17389f1bda3caaef31f7025
Sean <ussa.say@gmail.com>
2023-03-08 13:08:01 -0800

    Dump plan editor URLs to a file

--- dump_plans.py
+++ dump_plans.py
@@ -1,11 +1,27 @@
 """
 python3 dump_plans.py 2022 json
+python3 dump_plans.py 2022 html > reports/output/dump-plans.html
 """
 
 
 import json
 import sys
+from typing import List
+from urllib.parse import urlencode
 from parse import major_codes, major_plans
+from parse_defs import ProcessedCourse
+
+
+def to_json(courses: List[ProcessedCourse]):
+    return [
+        (
+            course.course_title,
+            int(course.units) if course.units % 1 == 0 else course.units,
+            3 if course.raw.overlaps_ge else 1 if course.for_major else 2,
+            course.term_index,
+        )
+        for course in courses
+    ]
 
 
 def dump_plans(year: int) -> None:
@@ -31,20 +47,7 @@ def dump_plans(year: int) -> None:
             for college_code in major_plan.colleges:
                 file.write(f',["{college_code}",')
                 json.dump(
-                    [
-                        [
-                            course.course_title,
-                            int(course.units)
-                            if course.units % 1 == 0
-                            else course.units,
-                            3
-                            if course.raw.overlaps_ge
-                            else 1
-                            if course.for_major
-                            else 2,
-                        ]
-                        for course in major_plan.plan(college_code)
-                    ],
+                    to_json(major_plan.plan(college_code)),
                     file,
                     separators=(",", ":"),
                 )
@@ -53,6 +56,40 @@ def dump_plans(year: int) -> None:
         file.write("]\n")
 
 
+def render_plan_urls(year: int) -> None:
+    all_plans = major_plans(year)
+    print("<dl>")
+    for major_plan in all_plans.values():
+        major_info = major_codes()[major_plan.major_code]
+        print(f"<dt>{major_plan.major_code}: {major_info.name}</dt><dd>")
+        for college_code in major_plan.colleges:
+            print(f'<a href="./plan-editor.html?')
+            print(
+                urlencode(
+                    {
+                        "year": year,
+                        "department": major_info.department,
+                        "major_name": major_info.name,
+                        "major": major_plan.major_code,
+                        "cipCode": major_info.cip_code,
+                        "collegeCode": college_code,
+                        "degreeType": list(major_info.award_types)[-1]
+                        if major_info.award_types
+                        else "BS",
+                        "courses": json.dumps(
+                            to_json(major_plan.plan(college_code)),
+                            separators=(",", ":"),
+                        ),
+                    }
+                )
+            )
+            print(f'">{college_code}</a> ')
+        print("</dd>")
+    print("</dl>")
+
+
 if __name__ == "__main__":
     if sys.argv[2] == "json":
         dump_plans(int(sys.argv[1]))
+    elif sys.argv[2] == "html":
+        render_plan_urls(int(sys.argv[1]))
