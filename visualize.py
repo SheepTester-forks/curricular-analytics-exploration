cb223fc5292bf07420788ed1dbb7659f221b8f16
Sean <ussa.say@gmail.com>
2024-01-11 13:12:04 -0800

    open degree plan in browser

---
+++ visualize.py
@@ -0,0 +1,33 @@
+"""
+Opens the graph visualization in the browser.
+
+python3 visualize.py <year> <major> <college>
+"""
+
+
+import json
+import webbrowser
+from urllib.parse import quote
+
+from output import MajorOutput
+from parse import major_plans
+
+BASE_URL = "https://educationalinnovation.ucsd.edu/_files/graph-demo.html?defaults=ca"
+
+
+def main(year: int, major: str, college: str):
+    url = (
+        BASE_URL
+        + "#"
+        + quote(json.dumps(MajorOutput(major_plans(year)[major]).output_json(college)))
+    )
+    print(url)
+    webbrowser.open(url)
+
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) < 4:
+        raise ValueError("Usage: python3 visualize.py <year> <major> <college>")
+    main(int(sys.argv[1]), sys.argv[2], sys.argv[3])
